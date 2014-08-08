0801f83a03bd20cf1f3977e3de9c7ec5a3316adf
Dmitry Ledentsov <dmlled@yahoo.com>
2014-08-08 15:35:39 +0200

    Update try_luabridge.cpp

--- try_luabridge.cpp
+++ try_luabridge.cpp
@@ -5,48 +5,48 @@
 #include <iostream>
 
 
+class Player {
+ int x;
+
+public:
+ Player() : x(0) { std::cout << __FUNCTION__ << std::endl; }
+
+ ~Player() { std::cout << __FUNCTION__ << std::endl; }
+
+ int get_x() const {
+  std::cout << __FUNCTION__ << std::endl;
+  return x;
+ }
+
+ void set_x(int x_) {
+  std::cout << __FUNCTION__ << " " << x_ << std::endl;
+  x = x_;
+ }
+
+ void jump() { std::cout << __FUNCTION__ << std::endl; }
+};
+
 void luabridge_bind(lua_State *L) {
- class Player {
-  int x;
-
- public:
-  Player() : x(0) { std::cout << __FUNCTION__ << std::endl; }
-
-  ~Player() { std::cout << __FUNCTION__ << std::endl; }
-
-  int get_x() const {
-   std::cout << __FUNCTION__ << std::endl;
-   return x;
-  }
-
-  void set_x(int x_) {
-   std::cout << __FUNCTION__ << " " << x_ << std::endl;
-   x = x_;
-  }
-
-  void jump() { std::cout << __FUNCTION__ << std::endl; }
- };
-
  luabridge::getGlobalNamespace(L)
  .beginClass<Player>("Player")
-        .addConstructor<void (*)(), RefCountedPtr<Player> /* creation policy */ >()
+ .addConstructor<void (*)(), RefCountedPtr<Player> /* creation policy */ >()
  .addProperty("x", &Player::get_x, &Player::set_x)
  .addFunction("jump", &Player::jump)
  .endClass()
- ;
-
-
- const char *test =
- "player = Player() \n"
- "player:jump() \n"
- "player.x = player.x + 3"
- ;
+ ; 
 }
 
 int main() {
  lua::State state;
+
  luabridge_bind(state.getState());
+
  try {
+  static const char *test =
+  "player = Player() \n"
+  "player:jump() \n"
+  "player.x = player.x + 3"
+  ;
   state.doString(test);
  }
  catch (std::exception &e) {
