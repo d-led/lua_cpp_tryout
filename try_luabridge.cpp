a45259f5ea29266f0003b362ba83a3415a480d80
Dmitry Ledentsov <dmlled@yahoo.com>
2014-08-08 15:30:08 +0200

    Update try_luabridge.cpp

--- try_luabridge.cpp
+++ try_luabridge.cpp
@@ -10,7 +10,7 @@ void luabridge_bind(lua_State *L) {
     int x;
 
   public:
-    Player() : x(0), y(0) { std::cout << __FUNCTION__ << std::endl; }
+    Player() : x(0) { std::cout << __FUNCTION__ << std::endl; }
 
     ~Player() { std::cout << __FUNCTION__ << std::endl; }
 
